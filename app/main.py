d129355486b6cee22a7800572cb423691577dacb
Vivek Dagar <vivekdagar2025@gmail.com>
2025-10-28 18:02:58 +0530

    respond to a ping

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment the code below to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept()  # Wait for a client to connect
+    connection.sendall(b"+PONG\r\n")        # Send Redis PONG response
 
 
 if __name__ == "__main__":
