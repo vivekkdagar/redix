ddda0a6448c0b6ff79684cff73f245be3e7d259d
Vivek Dagar <vivekdagar2025@gmail.com>
2025-10-28 18:06:01 +0530

    handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
 import socket
+import threading
+
+
+def handle_client(connection):
+    # Continuously handle multiple PINGs from this client
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break  # client disconnected
+        connection.sendall(b"+PONG\r\n")  # respond to each PING
+    connection.close()
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
 
-    # Continuously read and respond to PING commands
     while True:
-        data = connection.recv(1024)
-        if not data:
-            break  # client closed connection
-        connection.sendall(b"+PONG\r\n")  # hardcoded RESP response
+        connection, _ = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(connection,))
+        thread.start()
 
 
 if __name__ == "__main__":
